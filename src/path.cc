423e15f597a84f9e19d1f75588808f853a3e3c94
Daniel Lemire <daniel@lemire.me>
2024-03-25 21:10:31 -0400

    lint

--- src/path.cc
+++ src/path.cc
@@ -294,15 +294,14 @@ void ToNamespacedPath(Environment* env, BufferValue* path) {
                resolved_path2.size());
       }
     }
-  } else if (IsWindowsDeviceRoot(resolved_path[0]) &&
-               resolved_path[1] == ':' && resolved_path[2] == '\\') {
+  } else if (IsWindowsDeviceRoot(resolved_path[0]) && resolved_path[1] == ':' &&
+             resolved_path[2] == '\\') {
     // Matched device root, convert the path to a long UNC path
     std::string_view new_prefix = R"(\\?\")";
     path->SetLength(new_prefix.size() + resolved_path.size());
     memcpy(&path, new_prefix.data(), new_prefix.size());
-    memcpy(&path + new_prefix.size(),
-            resolved_path.data(),
-            resolved_path.size());
+    memcpy(
+        &path + new_prefix.size(), resolved_path.data(), resolved_path.size());
   }
 
 #endif
